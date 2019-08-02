418b1242b7aba0aec5957c2499c173f9c98db3d2
elyeel <elyeel@yahoo.com>
2019-08-02 00:42:13 +0000

    refactored and implemented index w/ m&c

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,32 @@
 const request = require('request');
-const arg = process.argv;
-const address = "https://api.thecatapi.com/v1/breeds/search?q=" + arg[2];
-console.time('Done in ');
-request(address, (error, response, body) => {
-  // console.log(body);
-// for any address error, body will contain not found
-  if (error) { // this is triggered by network error
-    console.log(error);
-    return;
-  } else {
-    if (JSON.parse(body).status >= 300) {
-      console.log(body);
+// const arg = process.argv;
+// const address = "https://api.thecatapi.com/v1/breeds/search?q=" + arg[2];
+// console.time('Done in ');
+
+const fetchBreedDescription = function(breedName, callback) {
+  const address = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(address, (error, response, body) => {
+    // console.log(body);
+    // for any address error, body will contain not found
+    if (error) { // this is triggered by network error
+      // console.log(error);
+      // return;
     } else {
-      console.timeEnd('Done in ');
-      const data = JSON.parse(body);
-      if (data.length === 0) {
-        console.log("Invalid input type of breed");
+      if (JSON.parse(body).status >= 300) {
+        // console.log(body);
       } else {
-        console.log(data[0].weight);
-        console.log(data[0].description);
+        // console.timeEnd('Done in ');
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          // console.log("Invalid input type of breed");
+        } else {
+          // console.log(data[0].weight);
+          callback(error, data[0].description);
+        }
       }
+      // console.log(typeof data); // data is actually an object
     }
-    // console.log(typeof data); // data is actually an object
-  }
-});
+  });
+};
+
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,11 @@
+const fetchBreedFetcher = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedFetcher(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details: ', error);
+  } else {
+    console.log(desc);
+  }
+});
